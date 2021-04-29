586660ff179b8f8692ce6f2029b8cc8b5d7b0e76
Marco Caizzi <marcocaizzi@hotmail.com>
2021-04-29 18:31:15 -0300

    Update

---
+++ login.feature
@@ -0,0 +1,9 @@
+# Created by MarcoCaizzi at 29/4/2021
+Feature: Administracion de usuario
+
+  Como usuario
+  Quiero tener diferentes versiones del menu principal de acuerdo al rol asignado al usuario
+  Para tener disgregacion de funciones de acuerdo al rol
+
+  Scenario: Validar usuario adminis
+    # Enter steps here
---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+    TITULO:Login
+
+    precondicion: Estar en la pag www.claro.com
+
+    pasos:
+    1) en el campo usuario, insertar el usuario 'admin'.
+    2) en el campo password, insertar la password.
+    3) dar clip en el boton ingresar.
+    4) Verificar se redirija a la pag correspondiente.
+    5) Comprobar que el label diga "administrador".
+
+
+tarea:
+crear un Readme.mb con una imagen y descripcion
+"""
